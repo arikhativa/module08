8c8383326b4abebefb02207b1e9ce57ad0f1ded4
Arik Hativa <arikhativa@gmail.com>
2023-06-20 10:55:53 +0300

    ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 16:42:11 by yrabby            #+#    #+#             */
-/*   Updated: 2023/06/19 16:53:09 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/06/20 10:52:45 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@
 # include <algorithm>
 
 template <typename T>
-typename T::const_iterator	easyfind(const T &container, const int i);
+typename T::const_iterator	easyfind(const T &container, const int i)
+{
+	typename T::const_iterator it;
+
+	it = std::find(container.begin(), container.end(), i);
+	if (it == container.end() && *it != i)
+		throw std::range_error ("failed to find element");
+	return it;
+}
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,28 +6,70 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 16:55:51 by yrabby            #+#    #+#             */
-/*   Updated: 2023/06/19 17:09:35 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/06/20 10:55:25 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <vector>
+#include <list>
 
 #include "easyfind.hpp"
 
 int main(void)
 {
-	std::vector<int> v(5);
+	{
+		std::vector<int> v(5);
+		v[0] = 1;
+		v[1] = 2;
+		v[2] = 2;
+		v[3] = 4;
+		v[4] = 5;
 
-	v[0] = 1;
-	v[1] = 1;
-	v[2] = 1;
-	v[3] = 1;
-	v[4] = 1;
-	v[55];
-	std::cout << v[222] << std::endl;
-	// std::vector<int>::const_iterator it = std::find(v.begin(), v.end(), 7);
-
-	// std::cout << *it << std::endl;
+		try
+		{
+			std::vector<int>::const_iterator it = easyfind(v, 2);
+			std::cout << *it << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		try
+		{
+			std::vector<int>::const_iterator it = easyfind(v, 7);
+			std::cout << *it << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	{
+		std::list<int> container;
+		container.push_back(1);
+		container.push_back(2);
+		container.push_back(3);
+		container.push_back(4);
+		
+		try
+		{
+			std::list<int>::const_iterator it = ::easyfind(container, 4);
+			std::cout << *it << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		try
+		{
+			std::list<int>::const_iterator it = ::easyfind(container, 7);
+			std::cout << *it << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
 	return 0;
 }
